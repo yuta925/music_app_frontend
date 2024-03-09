3e6ec81c6ff99ea3e95375b48213e8ba07f885fc
nki1121 <nkiuniv@gmail.com>
2024-03-10 03:33:51 +0900

    コンフリクト解決した

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import BulletinBoardPresenter from './components/features/bulletinBoard/bulletinBoardPresenter'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <TitlebarBelowImageList /> */}
-    <BulletinBoardPresenter />
+    <App />
   </React.StrictMode>
 )
