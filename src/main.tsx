70c7f7a32c0d35ad8b4f800511878a800cd84043
nki1121 <nkiuniv@gmail.com>
2024-03-10 12:31:42 +0900

    footer

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import Header from './ui/header/header'
+import Footer from './ui/footer/footer'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <App /> */}
     <Header />
+    <Footer />
   </React.StrictMode>
 )
--- src/ui/footer/footer.tsx
+++ src/ui/footer/footer.tsx
@@ -2,45 +2,61 @@ import { Box, IconButton } from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import EditNoteIcon from '@mui/icons-material/EditNote'
-// import IconButton from "@mui/material/IconButton";
 
 export default function Footer() {
   return (
     <Box
-      position="fixed"
       sx={{
-        //   '& button':{m: 1},
+        position: 'fixed',
         borderTop: '2px solid white',
         bottom: 0,
         left: 0,
         padding: 0,
         margin: 0,
         width: '100%',
-        height: 110,
-        borderRadius: 0,
-        bgcolor: 'primary.main',
-        '&:hover': {
-          bgcolor: 'primary.dark',
-        },
+        height: 68, // 「推奨値」らしい
+        backgroundColor: '#333333',
         display: 'flex',
         justifyContent: 'space-around',
         alignItems: 'center',
       }}
     >
+
       <IconButton
-        sx={{ color: 'white', '&:hover': { color: 'secondary.main' } }}
+        sx={{
+          color: '#FFFFFF',
+          '&:hover': {
+            color: '#EA4335'
+          }
+        }}
       >
-        <EditNoteIcon sx={{ fontSize: 70 }} />
+        <EditNoteIcon sx={{
+          fontSize: 70
+        }} />
       </IconButton>
       <IconButton
-        sx={{ color: 'white', '&:hover': { color: 'secondary.main' } }}
+        sx={{
+          color: '#FFFFFF',
+          '&:hover': {
+            color: '#EA4335'
+          }
+        }}
       >
-        <HomeIcon sx={{ fontSize: 70 }} />
+        <HomeIcon sx={{
+          fontSize: 70
+        }} />
       </IconButton>
       <IconButton
-        sx={{ color: 'white', '&:hover': { color: 'secondary.main' } }}
+        sx={{
+          color: '#FFFFFF',
+          '&:hover': {
+            color: '#EA4335'
+          }
+        }}
       >
-        <AccountCircleIcon sx={{ fontSize: 70 }} />
+        <AccountCircleIcon sx={{
+          fontSize: 70
+        }} />
       </IconButton>
     </Box>
   )
