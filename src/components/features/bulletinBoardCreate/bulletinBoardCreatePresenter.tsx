a1112033f1cc1624a156b47bcdacbce8b93a8d98
yuta925 <yutarou041@gmail.com>
2024-03-10 20:54:46 +0900

    header

--- src/components/features/bulletinBoardCreate/bulletinBoardCreatePresenter.tsx
+++ src/components/features/bulletinBoardCreate/bulletinBoardCreatePresenter.tsx
@@ -5,7 +5,7 @@ import { veneuOptions } from '../../../assets/Options/venueOptions'
 import { artistOptions } from '../../../assets/Options/artistOptions'
 import IconButton from '@mui/material/IconButton'
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'
-import Header from '../../../ui/header/header'
+import { Header2 } from '../../../ui/header/header2'
 import Footer from '../../../ui/footer/footer'
 
 type BulletinBoardCreateProps = {
@@ -32,10 +32,10 @@ export const BulletinBoardCreatePresenter: FC<BulletinBoardCreateProps> = ({
   submitHandler,
 }) => {
   return (
-    <div className='z-10'>
-      <Header />
+    <div className="z-10">
+      <Header2 />
       <form onSubmit={submitHandler}>
-         <div className="flex flex-col justify-center items-center mt-40">
+        <div className="flex flex-col justify-center items-center mt-40">
           <div className="flex flex-col justify-center items-center">
             <img src={live_image} className="h-40 w-40 rounded-lg " />
             <input
@@ -82,11 +82,9 @@ export const BulletinBoardCreatePresenter: FC<BulletinBoardCreateProps> = ({
               <AddCircleOutlineIcon sx={{ fontSize: 70, color: 'white' }} />
             </IconButton>
           </div>
-         </div>
+        </div>
       </form>
       <Footer />
-
     </div>
-   
   )
 }
