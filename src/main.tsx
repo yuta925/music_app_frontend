ad3cbbc73bd6fd3f16fbc423c70a2681637c1f82
nki1121 <nkiuniv@gmail.com>
2024-03-10 11:25:52 +0900

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Header from './ui/header/header'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Header />
   </React.StrictMode>
 )
--- src/ui/header/header.tsx
+++ src/ui/header/header.tsx
@@ -1,4 +1,3 @@
-import  React from 'react';
 import { Box,IconButton, Typography } from '@mui/material';
 import LogoutIcon from '@mui/icons-material/Logout';
 
